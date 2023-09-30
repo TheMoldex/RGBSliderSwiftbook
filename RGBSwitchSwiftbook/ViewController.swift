b038bd2422fee5e09b6c32dbd108f4ae7a49f68f
Denis Stashkov <themoldex@gmail.com>
2023-09-30 22:55:07 +0300

    add all constr

--- RGBSwitchSwiftbook/ViewController.swift
+++ RGBSwitchSwiftbook/ViewController.swift
@@ -7,17 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var changesView: UIView!
     
+    @IBOutlet weak var red: UILabel!
+    @IBOutlet weak var blue: UILabel!
+    @IBOutlet weak var green: UILabel!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingView()
     }
     
     override func viewWillLayoutSubviews() {
         view.backgroundColor = .systemMint
         changesView.layer.cornerRadius = changesView.frame.width / 2
+    }
+    
+    private func settingView() {
+        red.text = "Red: "
+        green.text = "Green: "
+        blue.text = "Blue: "
     }
 }
 
